e0df8bb2597020bdb7fea6dfe0dcd9b53b40a875
Toma <greebsasha24@gmail.com>
2024-08-12 01:42:56 +0300

    Deploy

--- src/config/settings/prod.py
+++ src/config/settings/prod.py
@@ -6,7 +6,7 @@ DEBUG = True
 
 SECRET_KEY = "django-secret-key"
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "ec2-13-60-202-186.eu-north-1.compute.amazonaws.com"]
 
 STATIC_ROOT = BASE_DIR / "static/"
 STATIC_URL = "/static/"
@@ -23,16 +23,16 @@ DATABASES = {
     #     "HOST": "localhost",
     #     "PORT": 5432,
     # },
-    # "default_sqlite": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # },
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_DB"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": os.environ.get("POSTGRES_HOST"),
-        "PORT": os.environ.get("POSTGRES_PORT"),
+    "default_sqlite": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     },
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": os.environ.get("POSTGRES_DB"),
+    #     "USER": os.environ.get("POSTGRES_USER"),
+    #     "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+    #     "HOST": os.environ.get("POSTGRES_HOST"),
+    #     "PORT": os.environ.get("POSTGRES_PORT"),
+    # },
 }
